fc079abb2f0d4ee1e2cf90a1d285b5e78a842e94
shtigran <shakhbekyantigran@gmail.com>
2017-02-19 20:03:17 +0400

    Example

---
+++ yieldReturnExample/yieldReturnExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yieldReturnExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Display powers of 2 up to the exponent of 8:
+            foreach (int i in Power(2, 8))
+            {
+                Console.Write("{0} ", i);
+            }
+
+            Console.ReadKey();
+        }
+
+        public static IEnumerable<int> Power(int number, int exponent)
+        {
+            int result = 1;
+
+            for (int i = 0; i < exponent; i++)
+            {
+                result = result * number;
+                yield return result;
+            }
+        }
+
+        // Output: 2 4 8 16 32 64 128 256
+    }
+}
